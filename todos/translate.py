aa0e08a5822e8067cb0c36573f294ac35cb45901
Victor Nieto <victornc83@gmail.com>
2021-02-09 23:45:09 +0000

    Añado llamada a la API de AWS Translate

--- todos/translate.py
+++ todos/translate.py
@@ -15,10 +15,11 @@ def translate(event, context):
     )
     
     translate = boto3.client(service_name='translate')
+    translated_text = translate.translate_text(Text=result["Item"]['text'], SourceLanguageCode="auto", TargetLanguageCode=event['pathParameters']['lang'])
     
     item = {
         'id': result["Item"]["id"],
-        'text': result["Item"]['text'],
+        'text': translated_text,
         'checked': result["Item"]["checked"],
         'createdAt': result["Item"]["createdAt"],
         'updatedAt': result["Item"]["updatedAt"]
